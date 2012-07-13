3cce7428129a2e5ecf06cc4353c06113046980f7
Mike De Haan <mikedehaan@gmail.com>
2012-07-12 23:28:15 -0500

    MAD - Commiting shell project

---
+++ mad-phenom/mad-phenom.cpp
@@ -0,0 +1,17 @@
+/*
+ * mad_phenom.cpp
+ *
+ * Created: 7/12/2012 11:26:12 PM
+ *  Author: mike
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}

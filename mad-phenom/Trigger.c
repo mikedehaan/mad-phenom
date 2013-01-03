2ae26b76fd9bb8ce65d69183a0912733b7edb954
Mike De Haan <mdehaan@psclistens.com>
2013-01-02 22:54:29 -0600

    Refactored Trigger to support new features
    Auto response will not fire a ball on release if trigger is held for 2+
    seconds.
    
    Ammo limit will reset if the trigger is held for 2+ seconds after the ammo limit
    has been reached.

--- mad-phenom/Trigger.c
+++ mad-phenom/Trigger.c
@@ -20,19 +20,21 @@ along with mad-phenom.  If not, see <http://www.gnu.org/licenses/>.
 #include "Solenoid.h"
 
 uint32_t trigger_activeTime = 0;
+uint32_t trigger_heldTime = 0;
 uint32_t queue_activeTime = 0;
 uint32_t lastTriggerPullTime = 0;
 uint8_t safetyShotsFired = 0;
 bool trigger_pulled = false;
 uint8_t firing_queue = 0;
 
-void trigger_singleShot(uint32_t *millis);
-void trigger_fullAuto(uint32_t *millis);
-void trigger_autoResponse(uint32_t *millis);
-void trigger_burst(uint32_t *millis);
+//void trigger_singleShot(uint32_t *millis);
+//void trigger_fullAuto(uint32_t *millis);
+//void trigger_autoResponse(uint32_t *millis);
+//void trigger_burst(uint32_t *millis);
 void fireFromQueue(uint32_t *millis);
+void trigger_action(uint32_t *millis);
 
-void (*fireMethod)(uint32_t *millis) = &trigger_fullAuto;
+void (*fireMethod)(uint32_t *millis) = &trigger_action;
 
 bool triggerHeld() {
 	return ((PINB & (1 << PINB2)) <= 0) || ((PINA & (1 << PINA6)) <= 0);
@@ -58,20 +60,24 @@ void trigger_changeMode() {
 	// Stop any active firing
 	trigger_pulled = false;
 	
+	/*
 	// 0 - Full Auto
 	// 1 - Burst
 	// 2 - Auto Response
-	if (FIRING_MODE == 0) {
+	if (FIRING_MODE == 0) {        // FULL AUTO
 		fireMethod = &trigger_fullAuto;
-	} else if (FIRING_MODE == 1) {
+	} else if (FIRING_MODE == 1) { // BURST
 		fireMethod = &trigger_burst;
-	} else if (FIRING_MODE == 2) {
+	} else if (FIRING_MODE == 2) { // AUTO RESPONSE
 		fireMethod = &trigger_autoResponse;
-	} else {
+	} else {                       // SINGLE SHOT
 		fireMethod = &trigger_singleShot;
 	}
+	*/
+	
+	fireMethod = &trigger_action;
 }
-
+/*
 void trigger_singleShot(uint32_t *millis) {
 
 	// Trigger Pulled
@@ -194,7 +200,96 @@ void trigger_burst(uint32_t *millis) {
 
 	fireFromQueue(millis);
 }
+*/
+void trigger_action(uint32_t *millis) {
+	
+	//////// TRIGGER PULLED
+	
+	// NOTE: Burst originally used checkPullDebounce()
+	if (!trigger_pulled && triggerHeld() &&	checkReleaseDebounce(millis)) {
 
+		trigger_pulled = true;
+		trigger_activeTime = (*millis);
+		trigger_heldTime = (*millis);  // How long the trigger was held for
+		
+		switch (FIRING_MODE) {
+			case 0: // Full Auto
+				firing_queue = 1;
+				break;
+			case 1: // Burst
+				// Don't allow burst if safety shots have not been reached
+				if (safetyShotsFired >= SAFETY_SHOT || SAFETY_SHOT == 0) {
+					firing_queue = BURST_SIZE;
+				} else {
+					firing_queue = 1;
+				}			
+				break;
+			case 2: // Auto Response
+				firing_queue++;			
+				break;
+			default:  // Single Shot
+				firing_queue = 1;			
+				break;
+		}
+	}
+		
+	//////// TRIGGER HELD
+	// Trigger Held
+	if (trigger_pulled && triggerHeld() && checkPullDebounce(millis) && (((*millis) - trigger_activeTime) >= ROUND_DELAY)) {
+		
+		switch (FIRING_MODE) {
+			case 0: // Full Auto
+				trigger_activeTime = (*millis);
+		
+				// Don't allow FA if safety shots have not been reached
+				// FA needs to be greater than the safety shot
+				// since holding the trigger would auto-qualify the last safety shot.
+				if (safetyShotsFired > SAFETY_SHOT || SAFETY_SHOT == 0) {
+					firing_queue = 1;
+				}
+				break;
+			case 1:
+				// Nothing while trigger is held
+				break;
+			case 2:
+				// Nothing while trigger is held
+				break;
+			default:
+				// Nothing while trigger is held
+				break;
+		}
+		
+		// If AMMO LIMIT is enabled and the trigger is held down for more than 2 seconds, reset the ammo limit
+		// For now, I'm leaving this enabled for full-auto as well (we'll see how the user feedback goes).
+		if (AMMO_LIMIT > 0 && shotsFired >= AMMO_LIMIT && ((*millis) - trigger_heldTime) >= 2000) {
+			// Reset the ammo limit
+			shotsFired = 0;
+		}
+	}
+	
+	//////// TRIGGER RELEASED
+	// Trigger Release
+	if (trigger_pulled && triggerReleased() && checkPullDebounce(millis)) {
+		trigger_pulled = false;
+		trigger_activeTime = (*millis);
+
+		// Fire a round if Auto response
+		if (FIRING_MODE == 2) {
+
+			// If the trigger was held for 2 seconds or more, don't fire a round
+			if (((*millis) - trigger_heldTime) < 2000) {
+
+				// Don't allow auto response if safety shots have not been reached
+				if (safetyShotsFired >= SAFETY_SHOT || SAFETY_SHOT == 0) {
+					firing_queue++;
+				}
+			}
+		}
+	}
+
+	// FIRE!!!
+	fireFromQueue(millis);	
+}
 
 // Semi Auto, set queue to 1
 // Full Auto, while the trigger is pulled, set queue to 1
